82d78519ede9876a79452e109c70ac04f8c6a85e
lance56 <58285987+lance56@users.noreply.github.com>
2021-03-08 21:47:50 +0800

    Update my_crontab_list.sh

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -107,4 +107,4 @@
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 #京小兑
-17 6,22 * * * node /scripts/jd_jxd.js >> /scripts/logs/jd_jxd.log 2>&1
+17 8,20 * * * node /scripts/jd_jxd.js >> /scripts/logs/jd_jxd.log 2>&1
